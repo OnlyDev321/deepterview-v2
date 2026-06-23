ff366c061ede3050fc8c78390c22202ccefbc21d
OnlyDev321 <onlydev.321@gmail.com>
2026-06-23 16:01:18 +0900

    fix CORS on production

--- backend/src/main/java/com/capstone/deepterview/global/config/SecurityConfig.java
+++ backend/src/main/java/com/capstone/deepterview/global/config/SecurityConfig.java
@@ -73,6 +73,8 @@ public class SecurityConfig {
 				"http://localhost:8080",
 				"http://localhost:5173",
 				"http://127.0.0.1:5173",
+				"http://deepterview.duckdns.org",
+				"https://deepterview.duckdns.org",
 				"http://deepterview.duckdns.org:5173",
 				"http://deepterview.duckdns.org:8080"
 		));
